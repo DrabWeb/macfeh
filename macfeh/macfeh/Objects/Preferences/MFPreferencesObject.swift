8bdcf722823efd04c4469158c26a1fe3c2ab79a2
DrabWeb <sethunity@gmail.com>
2018-01-12 18:22:31 -0400

    Cleanup MFPreferencesObject.swift

--- macfeh/macfeh/Objects/Preferences/MFPreferencesObject.swift
+++ macfeh/macfeh/Objects/Preferences/MFPreferencesObject.swift
@@ -7,31 +7,20 @@
 
 import Cocoa
 
-/// The object for storing preferences in macfeh
 class MFPreferencesObject: NSObject, NSCoding {
-    
-    // MARK: - Properties
-    
-    /// The preference for setting the default behaviour of the viewer's show background option
-    var viewerDefaultsShowBackground : Bool = true;
-    
-    /// The preference for setting the default behaviour of the viewer's enable shadow option
-    var viewerDefaultsEnableShadow : Bool = true;
-    
-    
-    // MARK: - Functions
-    
+
+    var viewerDefaultsShowBackground: Bool = true;
+    var viewerDefaultsEnableShadow: Bool = true;
+
     func encode(with coder: NSCoder) {
-        // Encode the preferences
         coder.encode(viewerDefaultsShowBackground, forKey: "viewerDefaultsShowBackground");
         coder.encode(viewerDefaultsEnableShadow, forKey: "viewerDefaultsEnableShadow");
     }
     
     required convenience init(coder decoder: NSCoder) {
-        self.init();
-        
-        // Decode and load the preferences
-        self.viewerDefaultsShowBackground = decoder.decodeBool(forKey: "viewerDefaultsShowBackground");
-        self.viewerDefaultsEnableShadow = decoder.decodeBool(forKey: "viewerDefaultsEnableShadow");
+        init();
+
+        viewerDefaultsShowBackground = decoder.decodeBool(forKey: "viewerDefaultsShowBackground");
+        viewerDefaultsEnableShadow = decoder.decodeBool(forKey: "viewerDefaultsEnableShadow");
     }
 }
